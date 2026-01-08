2fdf38ac36af39a8d1351c33029a9b3184cffc4e
tanthangkieu0013-maker <tanthangkieu0013@gmail.com>
2026-01-09 00:29:14 +0700

    feat: add initial C/C++ build task and sample program

---
+++ Thangws.c
@@ -0,0 +1,12 @@
+// Đây là dòng ghi chú
+/* Đây là dòng ghi chú
+Đây là dòng ghi chú
+Đây là dòng ghi chú
+Đây là dòng ghi chú
+*/
+#include <stdio.h>
+int main(){
+    printf("Hello, I'm Kieu Tan Thang!\n");
+    printf("Ma so sinh vien: PS47908\n");
+    return 0;
+}

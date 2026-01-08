ff2f54ef6ab5c51e0e1e298e7a8683988eea7951
tanthangkieu0013-maker <tanthangkieu0013@gmail.com>
2026-01-09 00:57:11 +0700

    feat: refactor Thangws.c to improve function organization and readability

--- Thangws.c
+++ Thangws.c
@@ -1,12 +1,38 @@
-// Đây là dòng ghi chú
-/* Đây là dòng ghi chú
-Đây là dòng ghi chú
-Đây là dòng ghi chú
-Đây là dòng ghi chú
-*/
 #include <stdio.h>
-int main(){
-    printf("Hello, I'm Kieu Tan Thang!\n");
-    printf("Ma so sinh vien: PS47908\n");
+// Function prototypes
+int function_example(void);
+void function_example2(void);
+
+int tinh_tong(int a, int b);
+
+// int = integer = số nguyên
+
+int main()
+{
+    int a = function_example();
+    function_example2();
+
+    int b = tinh_tong(5, 10);
+
+    printf("Gia tri duoc in ra la: %d\n", a);
+
     return 0;
+}
+
+int function_example()
+{
+    // This is an example function
+    printf("Hello, I'm Kieu Tan Thang!\n");
+    return 10;
+}
+
+void function_example2()
+{
+    // This is an example function
+    printf("Ma so sinh vien: PS47908\n");
+}
+
+int tinh_tong(int a, int b)
+{
+    return a + b;
 }
